e2f20e342e134dcd3249498f7f16009e1b0bba10
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-03-09 14:18:02 +0300

    Серега создал свою АПП все закончилось хорошо)  был малька рефактор/ можете меня ненавидеть теперь все =)   теперь TO4NO все   commit

--- src/n1_main/m1-ui/App.test.tsx
+++ src/n1_main/m1-ui/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import AppSerge from "./app/AppSerge";
 
+
+
 test('renders learn react link', () => {
-  render(<AppSerge />);
+  render(<AppSerge/>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
