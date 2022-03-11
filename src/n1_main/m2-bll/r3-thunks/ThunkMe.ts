6a94bf14b7e2eae56cee4d8add2dfa5df8053b5a
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-03-11 22:34:46 +0300

    ia ystal commit

--- src/n1_main/m2-bll/r3-thunks/ThunkMe.ts
+++ src/n1_main/m2-bll/r3-thunks/ThunkMe.ts
@@ -3,14 +3,17 @@ import {setAppStatusAC} from "../r1-reducers/app-reducer";
 import {meAPI} from "../../m3-dal/meAPI";
 import {ProfileActions} from "../r1-reducers/ProfileReducer";
 import {initializeMeAC, setErrorMeAC} from "../r2-actions/ActionsMe";
+import {LoginFormActions} from "../r2-actions/ActionLoginForm";
 
 export const meTC = () => async (dispatch: Dispatch) => {
     dispatch(setAppStatusAC("loading"))
     try {
+        debugger
         let res = await meAPI.me()
         dispatch(initializeMeAC(true))
         dispatch(ProfileActions.setProfileAC(res.data))
         // saveToken(res.data.token)
+        dispatch(LoginFormActions.setIsLoggedInAC(true))
         dispatch(setAppStatusAC("succeeded"))
     } catch (e: any) {
         if(e.response.data) {
