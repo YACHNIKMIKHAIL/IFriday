0f57a2f2a35e85bb8f9db82a67403dc4f74f787f
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-03-12 18:15:17 +0300

    FridayThunkType commit

--- src/n2_features/f2-packs&cards/b1-packs/packsTestComponent.tsx
+++ src/n2_features/f2-packs&cards/b1-packs/packsTestComponent.tsx
@@ -2,88 +2,88 @@ import React, {useState} from 'react';
 import {useDispatch} from "react-redux";
 import {packsActions} from "./ActionsPacks";
 import {useFridaySelector} from "../../../n1_main/m2-bll/store";
-import {packsTC} from "./ThunkPacks";
+import {addNewPacksTC, packsTC} from "./ThunkPacks";
 import {InitialCardPacksType} from "./packsReducer";
 
 
 const PacksTestComponent = () => {
-    const dispatch = useDispatch()
-    const myId = useFridaySelector<string>(state => state.profile.profile._id)
-    const packsState = useFridaySelector<InitialCardPacksType>(state => state.packs)
+        const dispatch = useDispatch()
+        const myId = useFridaySelector<string>(state => state.profile.profile._id)
+        const packsState = useFridaySelector<InitialCardPacksType>(state => state.packs)
 
-    const allMy = (value: string) => {
-        dispatch(packsActions.allMyAC(value))
-    }
-    const onChangeMin = (min: number) => {
-        dispatch(packsActions.minAC(min))
-    }
-    const onChangeMax = (max: number) => {
-        dispatch(packsActions.maxAC(max))
-    }
-    const search = (value: string) => {
-        dispatch(packsActions.searchAC(value))
-    }
-    const go = () => {
-        dispatch(packsTC())
-    }
-    const setPage = (page: number) => {
-        dispatch(packsActions.pageAC(page))
-    }
-    const setCardsPage = (cardsPage: number) => {
-        dispatch(packsActions.cardsPageAC(cardsPage))
-    }
+        const allMy = (value: string) => {
+            dispatch(packsActions.allMyAC(value))
+        }
+        const onChangeMin = (min: number) => {
+            dispatch(packsActions.minAC(min))
+        }
+        const onChangeMax = (max: number) => {
+            dispatch(packsActions.maxAC(max))
+        }
+        const search = (value: string) => {
+            dispatch(packsActions.searchAC(value))
+        }
+        const go = () => {
+            dispatch(packsTC())
+        }
+        const setPage = (page: number) => {
+            dispatch(packsActions.pageAC(page))
+        }
+        const setCardsPage = (cardsPage: number) => {
+            dispatch(packsActions.cardsPageAC(cardsPage))
+        }
 
-    console.log(packsState)
+        console.log(packsState)
 
-    const [newPackName,setNewPackName]=useState<string>('')
-    const [newPackPrivate,setNewPackPrivate]=useState<boolean>(false)
+        const [newPackName, setNewPackName] = useState<string>('')
+        const [newPackPrivate, setNewPackPrivate] = useState<boolean>(false)
 
-    const newPack = {
-        name: newPackName,
-        deckCover: '',
-        private: newPackPrivate
+        const newPack = {
+            name: newPackName,
+            deckCover: '',
+            private: newPackPrivate
+        }
+        const addNewPack = () => {
+            dispatch(addNewPacksTC(newPack))
+        }
+
+        return (
+            <div>
+                <div>
+                    <button onClick={() => allMy('')}>All</button>
+                    <button onClick={() => allMy(myId)}>My</button>
+                </div>
+                <div>
+                    min:
+                    <input type="number" onChange={(e) => onChangeMin(+e.currentTarget.value)}/>
+                    max:
+                    <input type="number" onChange={(e) => onChangeMax(+e.currentTarget.value)}/>
+                </div>
+                <div>
+                    search:
+                    <input type="text" onChange={(e) => search(e.currentTarget.value)}/>
+                    <button onClick={go}>go!</button>
+                </div>
+                <div>
+                    page:
+                    <input type="number" onChange={(e) => setPage(+e.currentTarget.value)}/>
+                </div>
+                <div>
+                    cards per page:
+                    <input type="number" onChange={(e) => setCardsPage(+e.currentTarget.value)}/>
+                </div>
+
+                <h1>New PACKS</h1>
+                <div>
+                    new pack name:
+                    <input type="text" onChange={(e) => setNewPackName(e.currentTarget.value)}/>
+                    private:
+                    <input type="checkbox" onChange={(e) => setNewPackPrivate(e.currentTarget.checked)}/>
+                    <button onClick={addNewPack}>add pack</button>
+                </div>
+            </div>
+        );
     }
-    const addNewPack=()=>{
-
-    }
-
-return (
-    <div>
-        <div>
-            <button onClick={() => allMy('')}>All</button>
-            <button onClick={() => allMy(myId)}>My</button>
-        </div>
-        <div>
-            min:
-            <input type="number" onChange={(e) => onChangeMin(+e.currentTarget.value)}/>
-            max:
-            <input type="number" onChange={(e) => onChangeMax(+e.currentTarget.value)}/>
-        </div>
-        <div>
-            search:
-            <input type="text" onChange={(e) => search(e.currentTarget.value)}/>
-            <button onClick={go}>go!</button>
-        </div>
-        <div>
-            page:
-            <input type="number" onChange={(e) => setPage(+e.currentTarget.value)}/>
-        </div>
-        <div>
-            cards per page:
-            <input type="number" onChange={(e) => setCardsPage(+e.currentTarget.value)}/>
-        </div>
-
-        <h1>New PACKS</h1>
-        <div>
-            new pack name:
-            <input type="text" onChange={(e)=>setNewPackName(e.currentTarget.value)}/>
-            private:
-            <input type="checkbox" onChange={(e)=>setNewPackPrivate(e.currentTarget.checked)}/>
-            <button>add pack</button>
-        </div>
-    </div>
-);
-}
 ;
 
 export default PacksTestComponent;
