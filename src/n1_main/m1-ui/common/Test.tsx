a2d47f76e056bd41b0204f57cca0437065af89ba
SergeyShapliuk <sergeshapluk@gmail.com>
2022-03-14 14:11:06 +0300

    cardsComponent

--- src/n1_main/m1-ui/common/Test.tsx
+++ src/n1_main/m1-ui/common/Test.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import PacksTestComponent from "../../../n2_features/f2-packs&cards_YM/b1-packs/packsTestComponent";
 import DoubleRange from "../doubleRange/DoubleRange";
 import PacksList from "../../../n2_features/f2-packs&cards_YM/b1-packs/PacksList";
+import CardsList from "../../../n2_features/f2-packs&cards_YM/b2-cards/CardsList";
 
 const Packs = () => {
     return (
         <div>
             {/*<PacksTestComponent/>*/}
             {/*<DoubleRange/>*/}
-            <PacksList/>
+            <CardsList/>
         </div>
     );
 };
