a37daa46f5b684bb73ecf6a938a8792793516d46
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-03-25 20:10:56 +0300

    Олежич бро причеши красиво =0  commit

--- src/n2_features/f2-packs&cards_YM/b1-packs/OnlyOnePackComponent.tsx
+++ src/n2_features/f2-packs&cards_YM/b1-packs/OnlyOnePackComponent.tsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './OnlyOnePackComponent.module.css'
+import style from './AddPackComponent.module.css'
 import {IconButton} from "@mui/material";
 import {Delete} from "@material-ui/icons";
 import {PackType} from "../../../n1_main/m2-bll/r1-reducers/packsReducer";
@@ -7,6 +8,11 @@ import {useFridaySelector} from "../../../n1_main/m2-bll/store";
 import Modal from "../../../n1_main/m1-ui/common/ModalWindow/ModalWindow";
 import EditPackComponent from "./EditPackComponent";
 import DeletePackComponent from "./DeletePackComponent";
+import {useDispatch} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {packsActions} from "../../../n1_main/m2-bll/r2-actions/ActionsPacks";
+import {cardsTC} from "../../../n1_main/m2-bll/r3-thunks/ThunkCards";
+import {RoutesXPaths} from "../../../n1_main/m1-ui/routes/routes";
 
 type OnlyOnePackComponentType = {
     item: PackType
@@ -15,7 +21,7 @@ type OnlyOnePackComponentType = {
 
 const OnlyOnePackComponent = ({item, runToCards}: OnlyOnePackComponentType) => {
 
-    const [mode, setMode] = useState<'edit'|'delete'|null>(null)
+    const [mode, setMode] = useState<'edit' | 'delete' | null | 'v'>(null)
 
     const myId = useFridaySelector<string>(state => state.profile.profile._id)
     const isLoad = useFridaySelector<boolean>(state => state.app.isLoad)
@@ -51,7 +57,7 @@ const OnlyOnePackComponent = ({item, runToCards}: OnlyOnePackComponentType) => {
                             </IconButton>
                         </div>
                         : <div className={s.BtnGroup__Item__My}>
-                            <button className={s.Btn} onClick={() => runToCards(item._id)} disabled={isLoad}>learn
+                            <button className={s.Btn} onClick={() => setMode('v')} disabled={isLoad}>learn
                             </button>
                         </div>
                 }
@@ -60,16 +66,69 @@ const OnlyOnePackComponent = ({item, runToCards}: OnlyOnePackComponentType) => {
                 backgroundOnClick={() => {
                     setMode(null)
                 }}
-                show={mode!==null}
+                show={mode !== null}
                 height={0}
                 width={0}
                 backgroundStyle={{backgroundColor: 'rgba(255,145,3,0.13)'}}
                 enableBackground={true}>
-                {mode==='delete'&&<DeletePackComponent id={item._id} setMode={()=>{setMode(null)}}/>}
-                {mode==='edit'&& <EditPackComponent item={item} closeModal={() => { setMode(null) }}/>}
+                {mode === 'delete' && <DeletePackComponent id={item._id} setMode={() => {
+                    setMode(null)
+                }}/>}
+                {mode === 'edit' && <EditPackComponent item={item} closeModal={() => {
+                    setMode(null)
+                }}/>}
+                {mode === 'v' && <SrazyIli packId={item._id} runToCards={runToCards} setMode={() => {
+                    setMode(null)
+                }}/>}
             </Modal>
+
         </div>
     )
 }
 
 export default OnlyOnePackComponent
+
+type SrazyIliType = {
+    runToCards: (packId: string) => void
+    packId: string
+    setMode: () => void
+}
+const SrazyIli = ({runToCards, packId, setMode}: SrazyIliType) => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const cardId = useFridaySelector<string>(state => state.cards.cards.filter(f => f.cardsPack_id === packId)[0]?._id)
+
+    useEffect(() => {
+        dispatch(cardsTC(packId))
+    }, [])
+
+    const xxx = () => {
+        dispatch(packsActions.allMyAC(''))
+        dispatch(packsActions.searchAC(''))
+        dispatch(packsActions.pageAC(1))
+        dispatch(packsActions.minAC(0))
+        dispatch(packsActions.maxAC(100))
+
+        setMode()
+        navigate(`${RoutesXPaths.LEARNED_CARD}/${packId}/${cardId}`)
+
+    }
+
+    return (
+        <div className={style.addItemContainer}>
+            <h2>
+                Do you go to cardsList or to learn ?
+            </h2>
+            <div className={style.centerInputContainer}>
+                    <span>
+                       Подумай дважды ? <span>&nbsp; ✎</span>
+                    </span>
+                <div>
+                    <button onClick={() => setMode()}>Cancel</button>
+                    <button onClick={() => runToCards(packId)}>To cardsList</button>
+                    <button onClick={xxx}>To learn</button>
+                </div>
+            </div>
+        </div>)
+
+}
